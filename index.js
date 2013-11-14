19068a07b68e5122cfc63ce573005674f8e990a0
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2013-11-13 22:50:38 -0500

    using from external module, fixes #3

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
-var username = 'bahmutov';
-
 var proud = require('proud');
 var badge = require('./src/badge');
 
-function total(counts) {
-  return Object.keys(counts).reduce(function (sum, name) {
-    return sum + counts[name];
-  }, 0);
+if (module.parent) {
+  module.exports = badge;
+} else {
+  var username = 'bahmutov';
+
+  function total(counts) {
+    return Object.keys(counts).reduce(function (sum, name) {
+      return sum + counts[name];
+    }, 0);
+  }
+
+  var filename = username + '.png';
+
+  proud(username)
+  .then(function (counts) {
+    var n = total(counts);
+    console.log(n + ' total downloads last month');
+    return badge(username, n, filename);
+  })
+  .catch(console.error)
+  .done(function () {
+    console.log('saved', filename);
+  });
 }
-
-var filename = username + '.png';
-
-proud(username)
-.then(function (counts) {
-  var n = total(counts);
-  console.log(n + ' total downloads last month');
-  return badge(username, n, filename);
-})
-.catch(console.error)
-.done(function () {
-  console.log('saved', filename);
-});
